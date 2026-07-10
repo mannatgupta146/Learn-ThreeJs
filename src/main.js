474fdc4444fec735f6e12d12ee0b1b703c20c5c6
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-11 00:26:05 +0530

    feat: initialize Three.js scene with animated cube and full-screen canvas styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,53 @@
 import './style.css'
+import * as THREE from "three"
+
+// scene
+const scene = new THREE.Scene()
+
+
+// camera 
+const camera = new THREE.PerspectiveCamera(
+    75, // field of view
+    window.innerWidth / window.innerHeight, // aspect ratio
+    0.01, // near
+    100 // far
+)
+
+camera.position.z = 5
+
+
+// mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+const material = new THREE.MeshBasicMaterial({
+  color: "red",
+})
+
+const cube = new THREE.Mesh(geometry, material)
+
+scene.add(cube)
+
+
+// canvas(big screen)
+const canvas = document.querySelector("canvas")
+
+
+// renderer(projector)
+const renderer = new THREE.WebGLRenderer({
+  canvas
+})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+
+
+// animation
+const animate = () => {
+  cube.rotation.y += 0.1
+
+  renderer.render(scene, camera)
+
+  requestAnimationFrame(animate)
+}
+
+animate()
